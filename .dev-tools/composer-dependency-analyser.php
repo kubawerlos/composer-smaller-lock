763950eca695c6c49ccf4738822970423ec6af24
Kuba Werłos <werlos@gmail.com>
2025-02-01 22:09:18 +0100

    Replace `maglnet/composer-require-checker` with `shipmonk/composer-dependency-analyser` (#35)

---
+++ .dev-tools/composer-dependency-analyser.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of composer-smaller-lock.
+ *
+ * (c) 2021 Kuba Werłos
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+$config = new Configuration();
+
+return $config
+    ->addPathToScan(__DIR__ . '/../src', false)
+    ->addPathToExclude(__DIR__ . '/../tests')
+    ->ignoreErrorsOnPackage('composer/composer', [ErrorType::DEV_DEPENDENCY_IN_PROD]);
